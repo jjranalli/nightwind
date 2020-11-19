0a6bb2ee8cc69d44ed297bb19458a8a2980f308e
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2020-11-19 04:02:35 +0100

    Tailwind 2.0 + gradients support

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 const plugin = require('tailwindcss/plugin')
 
 const nightwind = plugin(
-  function({ addComponents, theme, variants, e, prefix }) {
+  function({ addComponents, theme, variants }) {
         
     const darkSelector = theme('darkSelector', '.dark')
 
     const colorClasses = []
     const transitionClasses = []
-    
     const colors = theme('colors')
-    const colorVariants = variants('nightwind')
-    const prefixes = ['text', 'bg', 'border', 'placeholder', 'ring', 'ring-offset']
+    const colorVariants = ['hover']
+    const prefixes = ['text', 'bg', 'border']
     const weights = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900]
+    
+    if (variants('nightwind.variants')) {
+      colorVariants.push(...variants('nightwind.variants'))
+    }
+
+    if (variants('nightwind.colorClasses')) {
+      prefixes.push(...variants('nightwind.colorClasses'))
+      if (variants('nightwind.colorClasses').includes('gradient')) {
+        prefixes.push(...['from', 'via', 'to'])
+      }
+    }
 
     function hexToRGB(hex, alpha) {
       var r = parseInt(hex.slice(1, 3), 16),
@@ -107,40 +117,66 @@ const nightwind = plugin(
           return {
             [`${darkSelector} .${colorClass}${pseudoVariant ? `:${pseudoVariant}` : ''}`]: {
               color: colorClass.includes('white') ? theme('colors.black') : theme('colors.white'),
-              color: hexToRGB( `${colorClass.includes('white') ? '#000000' : '#ffffff'}` , 'var(--text-opacity)')
+              color: hexToRGB( `${colorClass.includes('white') ? '#000000' : '#ffffff'}` , 'var(--tw-text-opacity)')
             }
           }
         } else if (colorClass.includes('bg-')) {
           return {
             [`${darkSelector} .${colorClass}${pseudoVariant ? `:${pseudoVariant}` : ''}`]: {
               backgroundColor: colorClass.includes('white') ? theme('colors.black') : theme('colors.white'),
-              backgroundColor: hexToRGB( `${colorClass.includes('white') ? '#000000' : '#ffffff'}` , 'var(--bg-opacity)')
+              backgroundColor: hexToRGB( `${colorClass.includes('white') ? '#000000' : '#ffffff'}` , 'var(--tw-bg-opacity)')
             }
           }
         } else if (colorClass.includes('border-')) {
           return {
             [`${darkSelector} .${colorClass}${pseudoVariant ? `:${pseudoVariant}` : ''}`]: {
               borderColor: colorClass.includes('white') ? theme('colors.black') : theme('colors.white'),
-              borderColor: hexToRGB( `${colorClass.includes('white') ? '#000000' : '#ffffff'}` , 'var(--border-opacity)')
+              borderColor: hexToRGB( `${colorClass.includes('white') ? '#000000' : '#ffffff'}` , 'var(--tw-border-opacity)')
+            }
+          }
+        } else if (colorClass.includes('divide-')) {
+          return {
+            [`${darkSelector} .${colorClass}${pseudoVariant ? `:${pseudoVariant}` : ''} > :not([hidden]) ~ :not([hidden])`]: {
+              borderColor: colorClass.includes('white') ? theme('colors.black') : theme('colors.white'),
+              borderColor: hexToRGB( `${colorClass.includes('white') ? '#000000' : '#ffffff'}` , 'var(--tw-divide-opacity)')
             }
           }
         } else if (colorClass.includes('placeholder-')) {
           return {
             [`${darkSelector} .${colorClass}::placeholder`]: {
               color: colorClass.includes('white') ? theme('colors.black') : theme('colors.white'),
-              color: hexToRGB( `${colorClass.includes('white') ? '#000000' : '#ffffff'}` , 'var(--text-opacity)')
+              color: hexToRGB( `${colorClass.includes('white') ? '#000000' : '#ffffff'}` , 'var(--tw-text-opacity)')
             }
           }
         } else if (colorClass.includes('ring-offset-')) {
           return {
             [`${darkSelector} .${colorClass}${pseudoVariant ? `:${pseudoVariant}` : ''}`]: {
-              '--ring-offset-color': `${colorClass.includes('white') ? '#000000' : '#ffffff'}`
+              '--tw-ring-offset-color': `${colorClass.includes('white') ? '#000000' : '#ffffff'}`
             }
           }
         } else if (colorClass.includes('ring-')) {
           return {
             [`${darkSelector} .${colorClass}${pseudoVariant ? `:${pseudoVariant}` : ''}`]: {
-              '--ring-color': hexToRGB( `${colorClass.includes('white') ? '#000000' : '#ffffff'}` , 'var(--ring-opacity)')
+              '--tw-ring-color': hexToRGB( `${colorClass.includes('white') ? '#000000' : '#ffffff'}` , 'var(--tw-ring-opacity)')
+            }
+          }
+        } else if (colorClass.includes('from-')) {
+          return {
+            [`${darkSelector} .${colorClass}${pseudoVariant ? `:${pseudoVariant}` : ''}`]: {
+              '--tw-gradient-from': colorClass.includes('white') ? theme('colors.black') : theme('colors.white'),
+              '--tw-gradient-stops': `var(--tw-gradient-from), var(--tw-gradient-to, ${ hexToRGB(`${colorClass.includes('white') ? theme('colors.black') : theme('colors.white')}`, '0') })`,
+            }
+          }
+        } else if (colorClass.includes('via-')) {
+          return {
+            [`${darkSelector} .${colorClass}${pseudoVariant ? `:${pseudoVariant}` : ''}`]: {
+              '--tw-gradient-stops': `var(--tw-gradient-from), ${colorClass.includes('white') ? theme('colors.black') : theme('colors.white')}, var(--tw-gradient-to, ${hexToRGB(`${colorClass.includes('white') ? theme('colors.black') : theme('colors.white')}`, '0')})`,
+            }
+          }
+        } else if (colorClass.includes('to-')) {
+          return {
+            [`${darkSelector} .${colorClass}${pseudoVariant ? `:${pseudoVariant}` : ''}`]: {
+              '--tw-gradient-to': `${colorClass.includes('white') ? theme('colors.black') : theme('colors.white')}`,
             }
           }
         }
@@ -154,7 +190,7 @@ const nightwind = plugin(
           return {
             [`${darkSelector} .${colorClass}${pseudoVariant ? `:${pseudoVariant}` : ''}`]: {
               color: theme(`colors.${color}.${invertWeight}`),
-              color: hexToRGB( `${theme(`colors.${color}.${invertWeight}`)}` , 'var(--text-opacity)')
+              color: hexToRGB( `${theme(`colors.${color}.${invertWeight}`)}` , 'var(--tw-text-opacity)')
             }
           }  
         }
@@ -162,33 +198,59 @@ const nightwind = plugin(
           return {
             [`${darkSelector} .${colorClass}${pseudoVariant ? `:${pseudoVariant}` : ''}`]: {
               backgroundColor: theme(`colors.${color}.${invertWeight}`),
-              backgroundColor: hexToRGB( `${theme(`colors.${color}.${invertWeight}`)}` , 'var(--bg-opacity)')
+              backgroundColor: hexToRGB( `${theme(`colors.${color}.${invertWeight}`)}` , 'var(--tw-bg-opacity)')
             }
           }
         } else if (colorClass.includes('border-')) {
           return {
             [`${darkSelector} .${colorClass}${pseudoVariant ? `:${pseudoVariant}` : ''}`]: {
               borderColor: theme(`colors.${color}.${invertWeight}`),
-              borderColor: hexToRGB( `${theme(`colors.${color}.${invertWeight}`)}` , 'var(--border-opacity)')
+              borderColor: hexToRGB( `${theme(`colors.${color}.${invertWeight}`)}` , 'var(--tw-border-opacity)')
+            }
+          }
+        } else if (colorClass.includes('divide-')) {
+          return {
+            [`${darkSelector} .${colorClass}${pseudoVariant ? `:${pseudoVariant}` : ''} > :not([hidden]) ~ :not([hidden])`]: {
+              borderColor: theme(`colors.${color}.${invertWeight}`),
+              borderColor: hexToRGB( `${theme(`colors.${color}.${invertWeight}`)}` , 'var(--tw-divide-opacity)')
             }
           }
         } else if (colorClass.includes('placeholder-')) {
           return {
             [`${darkSelector} .${colorClass}::placeholder`]: {
               color: theme(`colors.${color}.${invertWeight}`),
-              color: hexToRGB( `${theme(`colors.${color}.${invertWeight}`)}` , 'var(--text-opacity)')
+              color: hexToRGB( `${theme(`colors.${color}.${invertWeight}`)}` , 'var(--tw-text-opacity)')
             }
           }
         } else if (colorClass.includes('ring-offset-')) {
           return {
             [`${darkSelector} .${colorClass}${pseudoVariant ? `:${pseudoVariant}` : ''}`]: {
-              '--ring-offset-color': `${theme(`colors.${color}.${invertWeight}`)}`
+              '--tw-ring-offset-color': `${theme(`colors.${color}.${invertWeight}`)}`
             }
           }
         } else if (colorClass.includes('ring-')) {
           return {
             [`${darkSelector} .${colorClass}${pseudoVariant ? `:${pseudoVariant}` : ''}`]: {
-              '--ring-color': hexToRGB( `${theme(`colors.${color}.${invertWeight}`)}` , 'var(--ring-opacity)'),
+              '--tw-ring-color': hexToRGB( `${theme(`colors.${color}.${invertWeight}`)}` , 'var(--tw-ring-opacity)'),
+            }
+          }
+        } else if (colorClass.includes('from-')) {
+          return {
+            [`${darkSelector} .${colorClass}${pseudoVariant ? `:${pseudoVariant}` : ''}`]: {
+              '--tw-gradient-from': `${theme(`colors.${color}.${invertWeight}`)}`,
+              '--tw-gradient-stops': `var(--tw-gradient-from), var(--tw-gradient-to, ${ hexToRGB(`${theme(`colors.${color}.${invertWeight}`)}`, '0') })`,
+            }
+          }
+        } else if (colorClass.includes('via-')) {
+          return {
+            [`${darkSelector} .${colorClass}${pseudoVariant ? `:${pseudoVariant}` : ''}`]: {
+              '--tw-gradient-stops': `var(--tw-gradient-from), ${theme(`colors.${color}.${invertWeight}`)}, var(--tw-gradient-to, ${hexToRGB(`${theme(`colors.${color}.${invertWeight}`)}`, '0')})`,
+            }
+          }
+        } else if (colorClass.includes('to-')) {
+          return {
+            [`${darkSelector} .${colorClass}${pseudoVariant ? `:${pseudoVariant}` : ''}`]: {
+              '--tw-gradient-to': `${theme(`colors.${color}.${invertWeight}`)}`,
             }
           }
         } 
