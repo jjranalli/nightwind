712ff73949783a4ef9da2b2a5631434fedbf0623
Milan Jaritz <33156526+macmillen@users.noreply.github.com>
2023-07-24 20:49:39 +0200

    support `950`-shades

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const nightwind = plugin(
     const colors = theme("colors")
     const colorVariants = ["hover"]
     const prefixes = ["text", "bg", "border"]
-    const weights = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900]
+    const weights = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 950]
     let importantSelector = ""
     let importantProperty = ""
 
@@ -151,16 +151,16 @@ const nightwind = plugin(
         const color = colorValues.pop()
         const defaultValue = theme(`colors.${color}.${weight}`)
 
-        let invertWeightIndex = 9 - weights.indexOf(Number(weight))
+        let invertWeightIndex = 10 - weights.indexOf(Number(weight))
         let invertWeight = String(weights[invertWeightIndex])
 
         if (theme("nightwind.colorScale.preset")) {
           switch (theme("nightwind.colorScale.preset")) {
             case "reduced":
               let reducedInvertWeightIndex =
-                10 - weights.indexOf(Number(weight))
-              reducedInvertWeightIndex > 9
-                ? (reducedInvertWeightIndex = 9)
+                11 - weights.indexOf(Number(weight))
+              reducedInvertWeightIndex > 10
+                ? (reducedInvertWeightIndex = 10)
                 : reducedInvertWeightIndex
               invertWeight = String(weights[reducedInvertWeightIndex])
               break
